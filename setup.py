fdf66f5ee9c021d9152a83f1c3e811703d31483d
useHTML5 <mne@sait-nado.ru>
2020-02-11 17:10:25 +0500

    add new theming

--- setup.py
+++ setup.py
@@ -3,24 +3,16 @@
 from setuptools import setup, find_packages
 from admin_tools import VERSION
 
-repo_url = 'https://github.com/django-admin-tools/django-admin-tools'
-long_desc = '''
-%s
-
-%s
-''' % (open('README.rst').read(), open('CHANGELOG').read())
 
 setup(
     name='django-admin-tools',
     version='0.2',
     description='A collection of tools for the django administration interface',
-    long_description=long_desc,
     long_description_content_type='text/x-rst',
     author='David Jean Louis',
     author_email='izimobil@gmail.com',
-    url=repo_url,
     download_url='https://pypi.python.org/packages/source/d/django-admin-tools/django-admin-tools-%s.tar.gz' % VERSION,
-    packages=find_packages(exclude=['test_proj*']),
+    packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     classifiers=[
